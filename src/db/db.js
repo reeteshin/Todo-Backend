1b83053b1441c43f8032ab342b8987eb004ad0f8
reetesh meena <reeteshmeena73@gmail.com>
2022-04-09 21:16:26 +0530

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const monggose = require("mongoose");
 
 const Connect = ()=>{
-    return monggose.connect(`mongodb+srv://1212:${process.env.PASS}@cluster0.kmsja.mongodb.net/todolist`)
+    return monggose.connect(`mongodb+srv://1212:1212@cluster0.kmsja.mongodb.net/todolist`)
 }
 
 module.exports  = Connect
